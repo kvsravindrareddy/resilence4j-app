94a498c51b46386e071b1f3115eda5a828cf7289
kvsravindrareddy <kvsravindrareddy@gmail.com>
2022-04-24 09:28:59 +0530

    RE123 changes

---
+++ src/test/java/com/trendingtech/Resilence4jAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendingtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilence4jAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
